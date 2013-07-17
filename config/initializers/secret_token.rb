2e79f2fc263aa1bbf5973c760824b5dc0974ccc8
Miguel <kanedaki@gmail.com>
2013-07-17 18:51:03 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Antahkarana::Application.config.secret_token = 'b425b48a713f339c8a8fdff8af52d63b09ce356a2de823b782d46a9aa6cf3e9d1ca2c23793a32635ef11c001a1453d2cd9ffc855b6c06f75744152123f602b20'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Antahkarana::Application.routes.draw do
+  root :to => 'pages#index'
+end
